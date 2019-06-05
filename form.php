f537e3e1d5479095e249935aa917769dff59f08b
Ivan <khlopetskiy.ivan@gmail.com>
2019-06-05 19:23:11 +0300

    first commit

--- form.php
+++ form.php
@@ -1,2 +1,21 @@
 <?php
-echo 1;
+
+$firstName = '';
+$lastName = '';
+$email = '';
+$password = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $parametrs = ['first_name', 'last_name', 'email', 'password'];
+
+    foreach ( $parametrs as $parametr) {
+        if ( empty($_POST[$parametr])) {
+            die('Не найден елемент ' . $parametr);
+        }
+    }
+
+    $firstName = $_POST['first_name'];
+    $lastName = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+}
